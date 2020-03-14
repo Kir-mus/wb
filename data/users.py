77017cbddf617f3842a5363fe552ae3a4615a83a
Kir-mus <zibox.one@mail.ru>
2020-03-14 18:17:41 +0300

    sqlachim 2

--- data/users.py
+++ data/users.py
@@ -1,15 +1,23 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    nam = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
@@ -18,3 +26,5 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     jobs = orm.relation("Jobs", back_populates='user')
+
+
--- main.py
+++ main.py
@@ -1,20 +1,102 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, login_user, login_required, logout_user
 from datetime import date
 from data import db_session
 from data.jobs import Jobs
+from data.users import User
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/jo.sqlite")
 db_session.global_init("db/blogs.sqlite")
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-session = db_session.create_session()
+class RegisterForm(FlaskForm):
+    email = EmailField('Login/email', validators=[DataRequired()])
+    password_1 = PasswordField('пароль', validators=[DataRequired()])
+    password_2 = PasswordField('еще раз', validators=[DataRequired()])
+    surname = StringField('surname', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    age = IntegerField('age', validators=[DataRequired()])
+    pos = StringField('Pos', validators=[DataRequired()])
+    spes = StringField('Spe', validators=[DataRequired()])
+    address = StringField('address', validators=[DataRequired()])
+    submit = SubmitField('setup')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password_1.data != form.password_2.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            position=form.pos.data,
+            speciality=form.spes.data,
+            address=form.address.data,
+            email=form.email.data,
+            age=form.age.data,
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/works')
 def works():
+    session = db_session.create_session()
     jobsid = {}
     for jobs in session.query(Jobs):
         jobsid[str(jobs.id)] = [jobs.job, jobs.team_leader, jobs.work_size, jobs.collaborators, jobs.is_finished]
